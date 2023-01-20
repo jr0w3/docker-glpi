16c37ba3efd15df1e55bdcada1269744d3e5fba8
jr0w3 <jonathan.languin@gmail.com>
2023-01-20 08:17:00 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+#Based Image
+FROM debian:11.6
+
+#Don't ask confirmation
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt update \
+&& apt install --yes --no-install-recommends \
+wget \
+ca-certificates \
+cron \
+apache2 \
+mariadb-server \
+perl \
+curl \
+jq \
+php \
+&& apt install --yes --no-install-recommends \
+php-ldap \
+php-imap \
+php-apcu \
+php-xmlrpc \
+php-cas \
+php-mysqli \
+php-mbstring \
+php-curl \
+php-gd \
+php-simplexml \
+php-xml \
+php-intl \
+php-zip \
+php-bz2 \
+&& rm -rf /var/lib/apt/lists/*
+
+# Copy entrypoint make it as executable and run it
+COPY entrypoint.sh /opt/
+RUN chmod +x /opt/entrypoint.sh
+ENTRYPOINT ["/opt/entrypoint.sh"]
+
+#Expose ports
+#EXPOSE 80 443
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [[ -d "/var/www/html/glpi" ]]
+then
+	echo "GLPI is already installed"
+else
+	wget -P /tmp/ https://github.com/glpi-project/glpi/releases/download/10.0.5/glpi-10.0.5.tgz
+	tar -xzf /tmp/glpi-10.0.5.tgz -C /var/www/html/
+	chown -R www-data:www-data /var/www/html/glpi
+	chmod -R 775 /var/www/html/glpi
+	rm -f /tmp/glpi-10.0.5.tgz
+fi
+
+#Setup vhost
+cat > /etc/apache2/sites-available/000-default.conf << EOF
+<VirtualHost *:80>
+        DocumentRoot /var/www/html/glpi
+
+        <Directory /var/www/html/glpi>
+                AllowOverride All
+                Order Allow,Deny
+                Allow from all
+        </Directory>
+
+        ErrorLog /var/log/apache2/error-glpi.log
+        LogLevel warn
+        CustomLog /var/log/apache2/access-glpi.log combined
+</VirtualHost>
+EOF
+
+# Setup Cron task
+echo "*/2 * * * * www-data /usr/bin/php /var/www/html/glpi/front/cron.php &>/dev/null" >> /etc/cron.d/glpi
+
+#Start cron service
+service cron start
+
+#Run apache in foreground mode.
+/usr/sbin/apache2ctl -D FOREGROUND
+
+#Activation du module rewrite d'apache
+a2enmod rewrite && service apache2 restart
