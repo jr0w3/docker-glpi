8d6b67999f512f0fac67d50b9183ed5409741449
jr0w3 <jonathan.languin@gmail.com>
2023-01-22 09:08:14 +0100

    Optimising

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,42 @@ then
 	echo "GLPI is already installed"
 else
 	mkdir -p /data/log
+	mkdir -p /data/frontend
+        mkdir -p /data/backend
+	mkdir -p /data/backend/config
 	wget -P /tmp/ https://github.com/glpi-project/glpi/releases/download/10.0.5/glpi-10.0.5.tgz
-	tar -xzf /tmp/glpi-10.0.5.tgz -C /data/
+	tar -xzf /tmp/glpi-10.0.5.tgz -C /data/frontend
+	mv data/frontend/glpi/files /data/backend/files 
+cat > /data/frontend/glpi/inc/downstream.php << EOF
+<?php
+define('GLPI_CONFIG_DIR', '/data/backend/config');
+
+if (file_exists(GLPI_CONFIG_DIR . '/local_define.php')) {
+   require_once GLPI_CONFIG_DIR . '/local_define.php';
+}
+EOF
+
+cat > /data/backend/config/local_define.php << EOF
+<?php
+define('GLPI_VAR_DIR', '/data/backend/files');
+define('GLPI_LOG_DIR', '/data/log');
+EOF
+
 	chown -R www-data:www-data /data
-	chmod -R 775 /data/glpi
+	chmod -R 775 /data
 	rm -f /tmp/glpi-10.0.5.tgz
-	cd /data/glpi
+	cd /data/frontend/glpi
 	php bin/console db:install --db-host=$MYSQL_HOST --db-name=$MYSQL_DATABASE --db-user=$MYSQL_USER --db-password=$MYSQL_PASSWORD --no-interaction
-	rm -rf /data/glpi/install
+	rm -rf /data/frontend/glpi/install
 fi
 
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
 #Setup vhost
 cat > /etc/apache2/sites-available/000-default.conf << EOF
 <VirtualHost *:80>
-        DocumentRoot /data/glpi
+        DocumentRoot /data/frontend/glpi
 
-        <Directory /data/glpi>
+        <Directory /data/frontend/glpi>
                 AllowOverride All
 		Require all granted
         </Directory>
@@ -31,7 +51,7 @@ cat > /etc/apache2/sites-available/000-default.conf << EOF
 EOF
 
 # Setup Cron task
-echo "*/2 * * * * www-data /usr/bin/php /data/glpi/front/cron.php &>/dev/null" >> /etc/cron.d/glpi
+echo "*/2 * * * * www-data /usr/bin/php /data/frontend/glpi/front/cron.php &>/dev/null" >> /etc/cron.d/glpi
 
 #Start cron service
 service cron start
